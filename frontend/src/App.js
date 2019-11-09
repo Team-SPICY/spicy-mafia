fd93c1d51d603e2ec6db072f2278c41dcbd1613d
Thuc Phan <tcp004@ucsd.edu>
2019-11-09 15:22:37 -0800

    Simple styling for in-game, added buttons and two buttons.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,22 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class App extends Component{
+
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="card">
+          <img className= "cardimg" src = "/images/card.png"/>
+        </div>
+        <div>
+          <button className = "buttontest">INSTRUCTIONS</button>
+          <button className = "buttontest2">PLAYER LIST</button>
+        </div>
+
+
       </div>
     );
   }
