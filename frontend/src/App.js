ae71c2bb67b4ba9d1ac11c4923d0d55272e7f75a
Thuc Phan <tcp004@ucsd.edu>
2019-11-16 14:43:50 -0800

    created the homescreen component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Image from 'react-bootstrap/Image';
-
+import Home from './components/Home/HomeScreen.js'
 class App extends Component{
 
   render() {
     return (
 
       <div className="App">
-        
-          <img className= "cardimg" src = "/images/card.png"/>
 
-        <div>
-          <button className = "buttontest">INSTRUCTIONS</button>
-          <button className = "buttontest2">PLAYER LIST</button>
-        </div>
-
+          <Home></Home>
 
       </div>
     );
--- frontend/src/components/Home/HomeScreen.js
+++ frontend/src/components/Home/HomeScreen.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
 import './HomeScreen.css';
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
 
-class Home extends Component{
+export class Home extends Component{
 
   render() {
     return (
 
       <div className="App">
-
-        
-
-
+        <img className = "homeLogo" src = "/images/MafiaHomeLogo.png"/>
+        <ButtonGroup className = "buttonGroupJH" vertical>
+            <Button className = "buttonHost">HOST</Button>
+            <Button className = "buttonJoin">JOIN</Button>
+        </ButtonGroup>
       </div>
     );
   }
