00d16ca46974f5f439c0cd25288371b2488984ab
YGrada <46659138+YGrada@users.noreply.github.com>
2019-11-16 12:14:38 -0800

    Update App.js
    Simplify it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Image from 'react-bootstrap/Image';
+import {Game} from './components/Game';
+import {Home} from "./components/Home/HomeScreen";
 
-class App extends Component{
+class App extends Component {
+    render() {
+            return (
+                <Game />
 
-  render() {
-    return (
-
-      <div className="App">
-        
-          <img className= "cardimg" src = "/images/card.png"/>
-
-        <div>
-          <button className = "buttontest">INSTRUCTIONS</button>
-          <button className = "buttontest2">PLAYER LIST</button>
-        </div>
-
-
-      </div>
-    );
-  }
+            );
+        }
 }
+
 export default App;
