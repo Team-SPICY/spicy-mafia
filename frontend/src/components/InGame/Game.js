c6e8a9935716ff4c9731e0372527dc53ad758e9a
YGrada <46659138+YGrada@users.noreply.github.com>
2019-11-18 13:17:11 -0800

    Update Game.js

--- frontend/src/components/InGame/Game.js
+++ frontend/src/components/InGame/Game.js
@@ -3,19 +3,29 @@ import Image from "react-bootstrap/Image";
 import "./Game.css";
 import {Button} from "react-bootstrap";
 import Player_List from "../Players/Player_list";
+import FlipCard from 'react-flipcard';
 
 export class Game extends Component{
     constructor(props){
         super(props);
-        this.state = {playersShow : false};
+        this.state = {playersShow : false,
+                      isDaytime   : false};
+
     }
 
     render(){
         return (
             <div className="Nightime">
 
-                <Image src="/images/card.png" width={" "} height = {"600"} className={"img"}/>
+                <FlipCard >
+                    <div className={"card_container"}>
+                        <Image src="/images/card.png" width={" "} height = {"600"} className={"img"}/>
+                    </div>
+                    <div >
+                        <Image src="/images/card.png" width={" "} height = {"600"}  />
+                    </div>
 
+                </FlipCard>
                 <div >
                     <button variant={"secondary"} type={"button"} className = "i_button">INSTRUCTIONS</button>
                     <button className = "p_button"
@@ -25,7 +35,6 @@ export class Game extends Component{
                         onHide = {() => this.setState({playersShow:false})}
                         />
                 </div>
-
             </div>
         );
 
