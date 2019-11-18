620edded2de533543f0ab266efbc32554163d437
YGrada <46659138+YGrada@users.noreply.github.com>
2019-11-18 13:47:39 -0800

    Update Game.js

--- frontend/src/components/InGame/Game.js
+++ frontend/src/components/InGame/Game.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Image from "react-bootstrap/Image";
-import Player_List from "./Player_list";
+import "./Game.css";
+import Player_List from "../Players/Player_list";
+import FlipCard from 'react-flipcard';
 
 export class Game extends Component{
     constructor(props){
@@ -10,11 +12,20 @@ export class Game extends Component{
 
     render(){
         return (
-            <div className="App">
-                <Image src="/images/card.png" width={" "} height={"600"} />
+            <div className="Nightime">
+
+                <FlipCard >
+                    <div >
+                        <Image src="/images/card.png" width={" "} height = {"600"} />
+                    </div>
+                    <div >
+                        <Image src="/images/card.png" width={" "} height = {"600"}  />
+                    </div>
+
+                </FlipCard>
                 <div >
-                    <button className="buttontest">INSTRUCTIONS</button>
-                    <button className="buttontest2"
+                    <button className = "i_button">INSTRUCTIONS</button>
+                    <button className = "p_button"
                             onClick={()=> this.setState({playersShow:true})}>PLAYER LIST</button>
                     <Player_List
                         show = {this.state.playersShow}
