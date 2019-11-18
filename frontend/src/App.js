bf606463cbf1bf9738a72d55c4275677cf0f6303
Thuc Phan <tcp004@ucsd.edu>
2019-11-18 12:42:08 -0800

    Added a field for user name. *need to implement alert for join

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Home from './components/Home/HomeScreen.js'
-class App extends Component{
 
-  render() {
-    return (
-
-      <div className="App">
-
-          <Home></Home>
-
-      </div>
-    );
-  }
-=======
-import {Game} from './components/Game';
-import {Home} from "./components/Home/HomeScreen";
+import {Home} from "./components/Home/HomeScreen.js";
 
 class App extends Component {
     render() {
             return (
-                <Game />
+              <div>
+                <Home/>
 
-            );
-        }
->>>>>>> 912b8425c4c4ee6dca7cf7d9b9f94ee11b9dcb12
+              </div>
+
+          );
+      }
 }
-
 export default App;
