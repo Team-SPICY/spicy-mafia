f90a2a381bfc98c73026ffb8f675c73b6c05659f
YGrada <46659138+YGrada@users.noreply.github.com>
2019-11-18 13:19:07 -0800

    Update Game.js

--- frontend/src/components/InGame/Game.js
+++ frontend/src/components/InGame/Game.js
@@ -1,43 +1,29 @@
 import React, {Component} from 'react';
 import Image from "react-bootstrap/Image";
-import "./Game.css";
-import {Button} from "react-bootstrap";
-import Player_List from "../Players/Player_list";
-import FlipCard from 'react-flipcard';
+import Player_List from "./Player_list";
 
 export class Game extends Component{
     constructor(props){
         super(props);
-        this.state = {playersShow : false,
-                      isDaytime   : false};
-
+        this.state = {playersShow : false};
     }
 
     render(){
         return (
-            <div className="Nightime">
-
-                <FlipCard >
-                    <div className={"card_container"}>
-                        <Image src="/images/card.png" width={" "} height = {"600"} className={"img"}/>
-                    </div>
-                    <div >
-                        <Image src="/images/card.png" width={" "} height = {"600"}  />
-                    </div>
-
-                </FlipCard>
+            <div className="App">
+                <Image src="/images/card.png" width={" "} height={"600"} />
                 <div >
-                    <button variant={"secondary"} type={"button"} className = "i_button">INSTRUCTIONS</button>
-                    <button className = "p_button"
+                    <button className="buttontest">INSTRUCTIONS</button>
+                    <button className="buttontest2"
                             onClick={()=> this.setState({playersShow:true})}>PLAYER LIST</button>
                     <Player_List
                         show = {this.state.playersShow}
                         onHide = {() => this.setState({playersShow:false})}
                         />
                 </div>
+
             </div>
         );
 
     }
 }
-
