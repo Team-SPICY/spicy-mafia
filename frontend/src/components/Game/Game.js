7fcf6c3167d5cd2500ca9947e13bb41e5dabbbde
brg029@ucsd.edu <brg029@ucsd.edu>
2019-11-18 20:51:46 -0800

    fixing last merge conflict

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import './Game.css';
 import axios from 'axios'
 import PlayerList from "./PlayerList";
-import FlipCard from 'react-flipcard';
-
+import FlipCard from '@kennethormandy/react-flipcard';
 import Image from "react-bootstrap/Image";
-
 import WebSocketInstance from '../../services/WebSocket'
 
 export default class Game extends Component {
