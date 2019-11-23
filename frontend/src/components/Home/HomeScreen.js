4619bc24d483442e3a4771eac10757ae40b9beaf
Thuc Phan <tcp004@ucsd.edu>
2019-11-22 20:33:20 -0800

    changes to homescreen

--- frontend/src/components/Home/HomeScreen.js
+++ frontend/src/components/Home/HomeScreen.js
@@ -14,7 +14,9 @@ export class Home extends Component {
             roomIDShow: false,
         };
     }
+
     render() {
+
         return (
             <div className="Homescreen">
 
@@ -24,26 +26,25 @@ export class Home extends Component {
                       <img className="homeLogo" src="/images/MafiaHomeLogo.png" />
                     </div>
 
+                    <label>USERNAME  </label>
                     <div className= "inputContainer">
-                    <div>
                       <div className="usernameInput">
-                          <label>USERNAME
+
                           <input type="text" name="name" type="text"
                                   onChange={this.props.usernameChangeHandler}
                                   placeholder="Who are you?"
                                   minLength="3"
                                   maxLength="10"
                               />
-                          </label>
+
                       </div>
                     </div>
-                    </div>
 
 
-                <div className ="buttonContainer">
-                  <Button onClick={() => this.props.handleCreateGame(this.props.username)} className="buttonHost">HOST</Button>
-                  <Button onClick={() => this.setState({ roomIDShow: true })} className="buttonJoin">JOIN</Button>
-                </div>
+                  <div className ="buttonContainer">
+                    <Button onClick={() => this.props.handleCreateGame(this.props.username)} className="buttonHost">HOST</Button>
+                    <Button onClick={() => this.setState({ roomIDShow: true })} className="buttonJoin">JOIN</Button>
+                  </div>
 
                 </div>
                 {this.state.roomIDShow ?
@@ -58,4 +59,5 @@ export class Home extends Component {
         );
     }
 }
+
 export default Home;
