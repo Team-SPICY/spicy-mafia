c0848ab35bd2290653bf973da6e325c1536249e9
YGrada <46659138+YGrada@users.noreply.github.com>
2019-12-01 13:08:14 -0800

    Update UserDayComponent.js

--- frontend/src/components/DayCycle/UserDayComponent.js
+++ frontend/src/components/DayCycle/UserDayComponent.js
@@ -6,7 +6,6 @@ import Image from "react-bootstrap/Image";
 //import WebSocketInstance from '../../services/WebSocket';
 import NarratorDay from '../UserComponents/NarratorDay';
 import DayVote from '../Vote/DayVote';
-import NewsFlash from "./NewsFlash";
 import Game from "../Game/Game";
 
 export default class UserDayComponent extends Component {
@@ -35,9 +34,9 @@ export default class UserDayComponent extends Component {
                             accused={this.props.accused}
                             currentUser={this.props.currentUser}
                             trialVotes={this.props.trialVotes}
-                            mafia_kill={this.props.mafia_kill}
-                            nurse_saved={this.props.nurse_saved}
-                            sheriff_inv={this.props.successful_investigation}
+                            mafia={this.props.mafia}
+                            nurse={this.props.nurse}
+                            sheriff ={this.props.sheriff}
                         />
                 }
             </div>
