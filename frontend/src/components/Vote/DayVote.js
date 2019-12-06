9660548f420c2f2b335f0ba7ef15f8e12a606340
YeniaG <ygradaib@ucsd.edu>
2019-12-05 21:18:29 -0800

    Update DayVote.js

--- frontend/src/components/Vote/DayVote.js
+++ frontend/src/components/Vote/DayVote.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Flipcard from '@kennethormandy/react-flipcard'
-
-import '@kennethormandy/react-flipcard/dist/Flipcard.css'
 
 import Image from 'react-bootstrap/Image';
 
@@ -16,37 +13,48 @@ import Instructions from "../Game/Instructions";
 import Game from "../Game/Game";
 import UserDayComponent from '../UserDayComponent/UserDayComponent';
 
+import PlayerList from "../Game/PlayerList";
+
+
 export default class DayVote extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-        }
+        this.state = {}
 
     }
 
-    // recieve vote and reflect that vote
+    // Recieve vote and reflect that vote
 
     render() {
+        const isAccused = this.props.accused === this.props.currentUser;
         return (
             <div className={"DayVote"}>
-                <h1>{this.props.currentUser}</h1>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Body>
+                <h1 className={"header"}>{this.props.currentUser}</h1>
+                <Card style = {{width: '30rem', height:'auto',verticalAlign:'middle'}}>
+                    <Card.Body bsPrefix={"body"}>
                         <Card.Title>IS {this.props.accused} GUILTY?</Card.Title>
                         <div>
-                            <Button onClick={() => WebSocketInstance.sendMessage({ 'command': 'on_trial_vote', playername: this.props.currentUser, vote: 'Guilty!' })}
-                                variant="secondary" size="lg" block>
+                            <Button onClick={() => WebSocketInstance.sendMessage({
+                                'command': 'on_trial_vote',
+                                playername: this.props.currentUser,
+                                vote: 'Guilty!'
+                            })}
+                                    variant="secondary" size="lg" block disabled={isAccused}>
                                 YES
                             </Button>
-                            <Button onClick={() => WebSocketInstance.sendMessage({ 'command': 'on_trial_vote', playername: this.props.currentUser, vote: 'Innocent!' })}
-                                variant="secondary" size="lg" block>
+                            <Button onClick={() => WebSocketInstance.sendMessage({
+                                'command': 'on_trial_vote',
+                                playername: this.props.currentUser,
+                                vote: 'Innocent!'
+                            })}
+                                    variant="secondary" size="lg" block disabled={isAccused}>
                                 NO
                             </Button>
                         </div>
                         <Form>
 
                             <fieldset className="formPeopleVotes">
-                                <Card.Title>THE PEOPLE SAY:</Card.Title>
+                                <Card.Title>CONSENSUS:</Card.Title>
                                 {
                                     Object.keys(this.props.trialVotes)
                                         .map((name) =>
@@ -57,23 +65,15 @@ export default class DayVote extends Component {
                         </Form>
                     </Card.Body>
                 </Card>
+                <div className="ingameButtonContainer">
+                  <Button onClick={() => this.setState({ instructionShow: true })} variant={"secondary"} type={"button"} className="instructionsButton">INSTRUCTIONS</Button>
+                    <Instructions
+                        show={this.state.instructionShow}
+                        onHide={() => this.setState({ instructionShow: false })}
+                    />
+                  <Button className="playerListButton" onClick={() => this.setState({ playersShow: true })}>PLAYER LIST</Button>
+                </div>
             </div>
         )
     }
-
 }
-
-/*
-return (
-    //flip card one side shows yes/no button, other side shows who voted yes who voted no
-    <Flipcard >
-        <div >
-            Side A
-        </div>
-        <div >
-            Side B
-        </div>
-
-    </Flipcard>
-)
-*/
